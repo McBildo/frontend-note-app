45e1142a17472fb789885780a25100f8069e8919
caio <caiosobreira@hotmail.com>
2022-06-26 11:11:47 -0300

    First commitof the project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import { Notification, Section } from "rbx";
+import './App.scss';
+import Road from './routes'
+
+
+const App = () => (
+  <Fragment>
+    <Road/>
+  </Fragment>
+
+)
+
+export default App;
